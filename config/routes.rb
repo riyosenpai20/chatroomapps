49eab7abf3d2c01a95677c12ab6256462ca361dd
riyo <riyosenpai20@gmail.com>
2022-12-10 06:54:00 +0700

    add page room

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :room_messages
   resources :rooms
+
+  root controller: :rooms, action: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
